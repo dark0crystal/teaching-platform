a26d96f344ea34b50a95518a0051c59ed5d55fb1
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-26 21:02:36 +0400

    design subject step

--- src/components/forms/GradeSubjectForm/SubjectStep2.tsx
+++ src/components/forms/GradeSubjectForm/SubjectStep2.tsx
@@ -1,7 +1,31 @@
-export default function SubjectStep2(){
-    return(
-        <div>
-            
-        </div>
-    )
+interface Props {
+  grade: string
+  subject: string
+  setFormData: React.Dispatch<React.SetStateAction<{ grade: string; subject: string }>>
+}
+
+export default function SubjectStep2({ grade, subject, setFormData }: Props) {
+  const subjectOptions = {
+    grade1: ["Math", "English"],
+    grade2: ["Science", "Arabic"],
+    grade3: ["Math", "Science", "History"],
+    grade4: ["Geography", "Islamic Studies"]
+  }
+
+  return (
+    <div>
+      <label>Select Subject for {grade}:</label>
+      <select
+        value={subject}
+        onChange={(e) => setFormData((prev) => ({ ...prev, subject: e.target.value }))}
+      >
+        <option value="">Select</option>
+        {subjectOptions[grade as keyof typeof subjectOptions]?.map((subj) => (
+          <option key={subj} value={subj}>
+            {subj}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
 }
