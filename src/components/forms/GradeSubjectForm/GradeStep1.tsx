1f0ba6ad0ff8794cf718785306115df16ab4e0b8
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-26 20:59:59 +0400

    design  step one

--- src/components/forms/GradeSubjectForm/GradeStep1.tsx
+++ src/components/forms/GradeSubjectForm/GradeStep1.tsx
@@ -1,14 +1,22 @@
-export default function GradeStep1(){
-    return(
-        <div>
-            <select>
-                <option value="">select</option>
-                <option value="grade1">Grade 1</option>
-                <option value="grade2">Grade 2</option>
-                 <option value="grade3">Grade 3</option>
-                <option value="grade4">Grade 4</option>
-            </select>
-            
-        </div>
-    )
+interface Props {
+  grade: string
+  setFormData: React.Dispatch<React.SetStateAction<{ grade: string; subject: string }>>
+}
+
+export default function GradeStep1({ grade, setFormData }: Props) {
+  return (
+    <div>
+      <label>Select Grade:</label>
+      <select
+        value={grade}
+        onChange={(e) => setFormData((prev) => ({ ...prev, grade: e.target.value }))}
+      >
+        <option value="">Select</option>
+        <option value="grade1">Grade 1</option>
+        <option value="grade2">Grade 2</option>
+        <option value="grade3">Grade 3</option>
+        <option value="grade4">Grade 4</option>
+      </select>
+    </div>
+  )
 }
