8680e3de6e00e9ebbe28a824a57b23ee2ab2aeaa
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-26 22:59:09 +0400

    hide the form if user already have chosen the grade and subject

--- src/app/[locale]/courses/page.tsx
+++ src/app/[locale]/courses/page.tsx
@@ -1,9 +1,29 @@
-import GradeSubjectFormWrapper from "@/components/forms/GradeSubjectForm/GradeSubjectWrapper";
+'use client'
 
-export default function Courses(){
-    return(
-        <div className="h-screen w-screen bg-green-50 flex justify-center items-center ">
-            <GradeSubjectFormWrapper/>
-        </div>  
-    )
+import { useEffect, useState } from "react"
+import GradeSubjectFormWrapper from "@/components/forms/GradeSubjectForm/GradeSubjectWrapper"
+
+export default function Courses() {
+  const [isFormVisible, setIsFormVisible] = useState(true)
+
+  useEffect(() => {
+    const grade = localStorage.getItem("grade")
+    const subject = localStorage.getItem("subject")
+
+    if (grade && subject) {
+      setIsFormVisible(false)
+    }
+  }, [])
+
+  return (
+    <div className="h-screen w-screen bg-green-50 flex justify-center items-center">
+      {isFormVisible ? (
+        <GradeSubjectFormWrapper />
+      ) : (
+        <div className="text-2xl text-gray-800">
+          ✅ You already selected <strong>{localStorage.getItem("grade")}</strong> - <strong>{localStorage.getItem("subject")}</strong>
+        </div>
+      )}
+    </div>
+  )
 }
