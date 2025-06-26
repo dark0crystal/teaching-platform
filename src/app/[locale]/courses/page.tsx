c7fb29f17b133767a4ba9b2a02db9c34f75ecae9
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-26 21:46:05 +0400

    center the form

--- src/app/[locale]/courses/page.tsx
+++ src/app/[locale]/courses/page.tsx
@@ -2,7 +2,7 @@ import GradeSubjectFormWrapper from "@/components/forms/GradeSubjectForm/GradeSu
 
 export default function Courses(){
     return(
-        <div className="h-screen w-screen bg-green-50">
+        <div className="h-screen w-screen bg-green-50 flex justify-center items-center ">
             <GradeSubjectFormWrapper/>
         </div>  
     )
