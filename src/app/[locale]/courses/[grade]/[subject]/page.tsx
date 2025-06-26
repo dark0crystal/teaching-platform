2dc74e5c5f7690cf4b661903bfc3255c2c9a51c2
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-26 20:50:18 +0400

    add grade step

---
+++ src/app/[locale]/courses/[grade]/[subject]/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Subject(){
+    return(
+        <div>
+
+        </div>
+    )
+}
---
+++ src/components/forms/GradeSubjectForm/GradeStep1.tsx
@@ -0,0 +1,14 @@
+export default function GradeStep1(){
+    return(
+        <div>
+            <select>
+                <option value="">select</option>
+                <option value="grade1">Grade 1</option>
+                <option value="grade2">Grade 2</option>
+                 <option value="grade3">Grade 3</option>
+                <option value="grade4">Grade 4</option>
+            </select>
+            
+        </div>
+    )
+}
