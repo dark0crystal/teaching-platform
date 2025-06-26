ea7a10981291bc7f38c1fc7b06ae567c9074885c
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-26 20:50:51 +0400

    use grade and subject form

---
+++ src/app/[locale]/courses/page.tsx
@@ -0,0 +1,9 @@
+import GradeSubjectFormWrapper from "@/components/forms/GradeSubjectForm/GradeSubjectWrapper";
+
+export default function Courses(){
+    return(
+        <div className="h-screen w-screen bg-green-50">
+            <GradeSubjectFormWrapper/>
+        </div>  
+    )
+}
