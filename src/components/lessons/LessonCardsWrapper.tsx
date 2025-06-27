f6c0f6089be6bbe166e411385d9500ceed4b1b31
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-27 21:55:31 +0400

    add a list of cards content for testing

--- src/components/lessons/LessonCardsWrapper.tsx
+++ src/components/lessons/LessonCardsWrapper.tsx
@@ -1,9 +1,39 @@
-import LessonCard from "./LessonCard";
+import LessonCard from "./LessonCard"
+import { LessonCardInterface } from "./lissonCardInterface"
 
-export default function LessonCardsWrapper(){
-    return(
-        <div>
-            <LessonCard lessonType="listening"/>
-        </div>
-    )
+export default function LessonCardsWrapper() {
+  const lessons: LessonCardInterface[] = [
+    {
+      lessonName: "Greetings",
+      lessonId: "lsn001",
+      lessonSlug: "greetings",
+      lessonType: "listening",
+    },
+    {
+      lessonName: "Basic Grammar",
+      lessonId: "lsn002",
+      lessonSlug: "basic-grammar",
+      lessonType: "reading",
+    },
+    {
+      lessonName: "Vocabulary Practice",
+      lessonId: "lsn003",
+      lessonSlug: "vocabulary-practice",
+      lessonType: "quiz",
+    },
+    {
+      lessonName: "Pronunciation",
+      lessonId: "lsn004",
+      lessonSlug: "pronunciation",
+      lessonType: "speaking",
+    },
+  ]
+
+  return (
+    <div className="grid grid-cols-1 gap-4 p-4">
+      {lessons.map((lesson) => (
+        <LessonCard key={lesson.lessonId} {...lesson} />
+      ))}
+    </div>
+  )
 }
