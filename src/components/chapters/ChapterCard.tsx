06af3bb97311c6064c903c7ec410f5409ab030ba
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-27 21:54:59 +0400

    make the width and height responsive

--- src/components/chapters/ChapterCard.tsx
+++ src/components/chapters/ChapterCard.tsx
@@ -4,7 +4,7 @@ import { ChapterCardInterface } from "./ChapterCardInterface";
 
 export default function ChapterCard({ chapterNumber, chapterName }: ChapterCardInterface) {
   return (
-    <div className="bg-white shadow-md rounded-3xl p-6 w-72 h-40 flex flex-col justify-between items-center border-2 border-blue-200 hover:shadow-xl transition-all">
+    <div className="bg-white shadow-md rounded-3xl p-6 w-fit h-fit flex flex-col justify-between items-center border-2 border-blue-200 hover:shadow-xl transition-all">
       <div className="flex items-center space-x-4">
         <div className="bg-blue-100 text-blue-700 font-bold text-lg w-12 h-12 flex items-center justify-center rounded-full">
           {chapterNumber}
