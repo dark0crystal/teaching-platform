8db6d2f9da2c6484a8a57104900077c949b3d19e
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-27 21:50:12 +0400

    cards wrapper

---
+++ src/components/lessons/LessonCardsWrapper.tsx
@@ -0,0 +1,9 @@
+import LessonCard from "./LessonCard";
+
+export default function LessonCardsWrapper(){
+    return(
+        <div>
+            <LessonCard lessonType="listening"/>
+        </div>
+    )
+}
