0737bf2792048e5f3b2a8a715f7914dc9f3292d6
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-28 08:44:38 +0400

    fix styling

--- src/components/lessons/LessonCard.tsx
+++ src/components/lessons/LessonCard.tsx
@@ -4,7 +4,7 @@ export default function LessonCard(lessonCardProps:LessonCardInterface){
     return(
         <div>
             <div className="rounded-full min-w-[140px] min-h-[140px] bg-gray-400">
-                <div className="rounded-full min-w-[140px] min-h-[140px] bg-green-300">
+                <div className="rounded-full min-w-[120px] min-h-[120px] bg-green-300">
                    {lessonCardProps.lessonType}
                 </div> 
             </div>
