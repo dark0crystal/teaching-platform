f5e0253d0fb56d39e0c659ec04ef406a15a3343a
ALMardasDEV <albusaidi9094@gmail.com>
2025-07-18 17:18:08 +0400

    add link

--- frontend/src/components/lessons/LessonCard.tsx
+++ frontend/src/components/lessons/LessonCard.tsx
@@ -1,13 +1,15 @@
 import { LessonCardInterface } from "./lissonCardInterface";
-
+import { Link } from "@/i18n/navigation";
 export default function LessonCard(lessonCardProps:LessonCardInterface){
     return(
         <div>
-            <div className="rounded-full min-w-[140px] min-h-[140px] bg-gray-400">
-                <div className="rounded-full min-w-[120px] min-h-[120px] bg-green-300">
-                   {lessonCardProps.lessonType}
-                </div> 
-            </div>
+            <Link href="/">
+                <div className="rounded-full min-w-[140px] min-h-[140px] bg-gray-400">
+                    <div className="rounded-full min-w-[120px] min-h-[120px] bg-green-300">
+                    {lessonCardProps.lessonType}
+                    </div> 
+                </div>
+            </Link>
         </div>
     )
 }
