809cfed3fba9077f2eee53d8bb7d592eaa48dd96
ALMardasDEV <albusaidi9094@gmail.com>
2025-07-31 22:32:01 +0400

    fix typo

--- frontend/src/components/lessons/LessonCard.tsx
+++ frontend/src/components/lessons/LessonCard.tsx
@@ -5,10 +5,11 @@ export default function LessonCard(lessonCardProps:LessonCardInterface){
     return(
         <div>
             {/* should navigate to lesson part page */}
-            <Link href={`courses//${nav}`}>
+            <Link href={`lessons/${nav}`}>
                 <div className="rounded-full min-w-[140px] min-h-[140px] bg-gray-400">
                     <div className="rounded-full min-w-[120px] min-h-[120px] bg-green-300">
                     {lessonCardProps.lessonType}
+               
                     </div> 
                 </div>
             </Link>
