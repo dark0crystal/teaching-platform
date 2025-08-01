e1acb9708b41a1e04e4d608dc846a20ebab324f6
ALMardasDEV <albusaidi9094@gmail.com>
2025-08-01 16:56:09 +0400

    adding a sample question for testing

--- frontend/src/components/lessons/LessonCard.tsx
+++ frontend/src/components/lessons/LessonCard.tsx
@@ -1,6 +1,19 @@
+import MultipleChoiceMultiple from "../questions/MultipleChoiceMultiple";
 import { LessonCardInterface } from "./lissonCardInterface";
 import { Link } from "@/i18n/navigation";
 export default function LessonCard(lessonCardProps:LessonCardInterface){
+
+const sampleQuestion = {
+  id: 'q1',
+  type: 'multiple_choice_multiple',
+  question: 'Which of the following are fruits?',
+  choices: ['Carrot', 'Apple', 'Potato', 'Banana', 'Tomato'],
+  correctAnswers: ['Apple', 'Banana', 'Tomato']
+};
+const handleAnswer = (isCorrect: boolean, selected: string[]) => {
+    console.log('User answered:', selected, 'Correct:', isCorrect);
+  };
+
     const nav = "part1"
     return(
         <div>
@@ -9,7 +22,7 @@ export default function LessonCard(lessonCardProps:LessonCardInterface){
                 <div className="rounded-full min-w-[140px] min-h-[140px] bg-gray-400">
                     <div className="rounded-full min-w-[120px] min-h-[120px] bg-green-300">
                     {lessonCardProps.lessonType}
-               
+
                     </div> 
                 </div>
             </Link>
